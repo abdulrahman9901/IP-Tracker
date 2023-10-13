6c339317f78c4a56049d4835a3df3bbba04342b6
Abdulrahman Badr <41681016+abdulrahman9901@users.noreply.github.com>
2023-10-13 23:06:13 +0300

    fix marker issue

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 
-const svgMarkup = '<svg xmlns="http://www.w3.org/2000/svg" width="46" height="56"><path fill-rule="evenodd" d="M39.263 7.673c8.897 8.812 8.966 23.168.153 32.065l-.153.153L23 56 6.737 39.89C-2.16 31.079-2.23 16.723 6.584 7.826l.153-.152c9.007-8.922 23.52-8.922 32.526 0zM23 14.435c-5.211 0-9.436 4.185-9.436 9.347S17.79 33.128 23 33.128s9.436-4.184 9.436-9.346S28.21 14.435 23 14.435z"/></svg>';
-map.setCenter({ lat: 34.05430, lng: -118.08212 });
-var bearsIcon = new H.map.Icon(svgMarkup);
-bearsMarker = new H.map.Marker({lat: 34.05430, lng: -118.08212 },{icon: bearsIcon});
+
 
 function fetchIpData(ip) {
   fetch(`http://api.ipstack.com/${ip}?access_key=cdc36e56c18d724e0bdc06e30be4f1b1`)
@@ -80,6 +77,7 @@ function updateMap(latitude, longitude) {
       });
       var defaultLayers = platform.createDefaultLayers();
       //Step 2: initialize a map - this map is centered over Europe
+      const svgMarkup = '<svg xmlns="http://www.w3.org/2000/svg" width="46" height="56"><path fill-rule="evenodd" d="M39.263 7.673c8.897 8.812 8.966 23.168.153 32.065l-.153.153L23 56 6.737 39.89C-2.16 31.079-2.23 16.723 6.584 7.826l.153-.152c9.007-8.922 23.52-8.922 32.526 0zM23 14.435c-5.211 0-9.436 4.185-9.436 9.347S17.79 33.128 23 33.128s9.436-4.184 9.436-9.346S28.21 14.435 23 14.435z"/></svg>';
       var map = new H.Map(document.getElementById('map'),
           defaultLayers.vector.normal.map,
           {
@@ -88,6 +86,8 @@ function updateMap(latitude, longitude) {
               pixelRatio: window.devicePixelRatio || 1
           }
       );
+      var bearsIcon = new H.map.Icon(svgMarkup);
+      bearsMarker = new H.map.Marker({ lat: 37.404319763183594, lng: -122.16726684570312},{icon: bearsIcon});
       // This adds a resize listener to make sure that the map occupies the whole container
       window.addEventListener('resize', () => map.getViewPort().resize());
       //Step 3: make the map interactive
