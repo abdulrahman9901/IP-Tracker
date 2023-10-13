15f39f8a6baf35050a7eebb617a2f1d4839a3821
Abdulrahman Badr <41681016+abdulrahman9901@users.noreply.github.com>
2023-10-13 23:23:28 +0300

    redeclaration issue.

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ searchBtn.addEventListener('click', function(e) {
 const ip = document.querySelector('#searchbar').value ;
 fetchIpData(ip);
 });
-const svgMarkup = '<svg xmlns="http://www.w3.org/2000/svg" width="46" height="56"><path fill-rule="evenodd" d="M39.263 7.673c8.897 8.812 8.966 23.168.153 32.065l-.153.153L23 56 6.737 39.89C-2.16 31.079-2.23 16.723 6.584 7.826l.153-.152c9.007-8.922 23.52-8.922 32.526 0zM23 14.435c-5.211 0-9.436 4.185-9.436 9.347S17.79 33.128 23 33.128s9.436-4.184 9.436-9.346S28.21 14.435 23 14.435z"/></svg>';
    // Function to update the map with new coordinates
    function updateMap(latitude, longitude) {
     try {
